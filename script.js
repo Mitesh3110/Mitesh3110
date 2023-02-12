579c72b1d0a3334b94864738deef12bb3a00e0a6
MiteshMakvana <>
2023-02-12 08:51:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = document.getElementsByTagName('div')[0];
+// a.innerHTML = a.innerHTML + "<h1>Hello duniiya !</h1>"
+// used for insertion
+
+let div = document.createElement('div');
+div.innerHTML = "<h1> Hello...World...!</h1>";
+// a.appendChild(div);
+// a.prepend(div)
+// a.after(div);
+// a.before(div);
+// a.replaceWith(div);
